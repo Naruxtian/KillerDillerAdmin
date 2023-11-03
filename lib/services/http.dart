58faabfd10f5cd892feda9a0770b9f423052db5a
IDGS-803-20001510 <alexis_prado_ojeda@hotmail.com>
2023-11-03 16:30:19 -0600

    cambios

--- lib/services/http.dart
+++ lib/services/http.dart
@@ -2,15 +2,15 @@
 import 'package:http/http.dart' as http;
 
 Future<String> urlServicePost(String path, Map<dynamic, dynamic> body) async {
-  // var url = Uri.http('localhost:3000', '/api/$path');
-  var url = Uri.http('13.56.233.124:3000', '/api/$path');
+  var url = Uri.http('localhost:3000', '/api/$path');
+  // var url = Uri.http('13.56.233.124:3000', '/api/$path');
   var response = await http.post(url, body: body);
   return response.body;
 }
 
 Future<int> urlServiceDelete(String path, String id) async {
-  // var url = Uri.http('localhost:3000', '/api/$path');
-  var url = Uri.http('13.56.233.124:3000', '/api/$path');
+  var url = Uri.http('localhost:3000', '/api/$path');
+  // var url = Uri.http('13.56.233.124:3000', '/api/$path');
   var response = await http.delete(url, body: id);
   return response.statusCode;
 }
@@ -19,8 +19,8 @@ Future<String> urlServiceGet(
   String path,
 ) async {
   try {
-    // var url = Uri.http('localhost:3000', '/api/$path');
-    var url = Uri.http('13.56.233.124:3000', '/api/$path');
+    var url = Uri.http('localhost:3000', '/api/$path');
+    // var url = Uri.http('13.56.233.124:3000', '/api/$path');
     var response = await http.get(
       url,
     );
